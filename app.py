4fcf4fe4e08c32faf05a5ad30e4c878d54fefdd9
Hyungju Moon <splex7@users.noreply.github.com>
2024-07-02 23:25:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "안녕하세요! CI/CD 파이프라인에 오신 것을 환영합니다!"
+    return "안녕하세요! CI/CD 파이프라인에 오신 것을 진심으로 환영합니다!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
